edf0511313c28c31b6de5ba97003697e7a612cc4
Enzzo Cavallo <souenzzo@gmail.com>
2016-09-25 22:49:59 -0300

    WIP

--- nomade.py
+++ nomade.py
@@ -4,6 +4,8 @@
 class nomade:
     def __init__(self, state = {}):
         self.state = state
+    def savedeps(self, prefix = "."):
+        pass
     def run(self):
         return {}
 
@@ -20,24 +22,50 @@ class ping(nomade):
         return self.state
 
 class ssh(nomade):
-    def run(self):
-        from subprocess import run
-        from pickle import dump, load
-        target = self.state["target"]
-        task = self.state["task"]
-        with open("./task.pyc", "bw") as f:
+    def dumpState(self, prefix = "."):
+        task.savedeps(prefix)
+        mkdir("%s/state"%(prefix))
+        with open("%s/state/task.pyc"%(prefix), "bw") as f:
             dump(task, f)
-        rsync = ["rsync", "-a", "--delete", "../nomade", "%s:"%(target)]
+
+    def sendState(self, prefix, target):
+        rsync = ["rsync"
+                , "-a"
+                , "--delete"
+                , "%s"%(prefix)
+                , "%s:"%(target)]
         run(rsync)
 
+    def remoteRun(self, prefix, target):
         ssh = ["ssh", "%s"%(target), "cd nomade && python3 -m nomade task.pyc state.pyc"]
         run(ssh)
-
+    def getState(self, prefix, target):
         rsync = ["rsync", "-a", "--delete", "%s:nomade/state.pyc"%(target), "."]
         run(rsync)
 
+    def restoreState(self, prefix):
         with open('./state.pyc', 'br') as f:
             res = load(f)
+        return res
+    
+    def run(self):
+        from subprocess import run
+        from pickle import dump, load
+        from tempfile import mkdtemp
+        from os import mkdir
+
+        target = self.state["target"]
+        task = self.state["task"]
+
+        prefix = mkdtemp()
+        self.dumpState(prefix)
+
+        self.sendState(prefix, target)
+        self.remoteRun(prefix, target)
+        self.getState(prefix, target)
+
+        res = self.restoreState(prefix)
+
         return { "status": "ok", "task": res }
 
 
